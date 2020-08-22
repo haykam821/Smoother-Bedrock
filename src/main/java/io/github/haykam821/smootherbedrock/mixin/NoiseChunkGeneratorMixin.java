e6eeb7e8047399d5549dbe1d16633affec78dec4
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-08-22 12:47:37 -0400

    Implement basic functionality

---
+++ src/main/java/io/github/haykam821/smootherbedrock/mixin/NoiseChunkGeneratorMixin.java
@@ -0,0 +1,20 @@
+package io.github.haykam821.smootherbedrock.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
+
+@Mixin(NoiseChunkGenerator.class)
+public class NoiseChunkGeneratorMixin {
+	@ModifyConstant(method = "buildBedrock", constant = @Constant(intValue = 5))
+	private int changeRoofLoopCount(int value) {
+		return 1;
+	}
+
+	@ModifyConstant(method = "buildBedrock", constant = @Constant(intValue = 4, ordinal = 2))
+	private int changeFloorLoopInitialValue(int value) {
+		return 0;
+	}
+}
