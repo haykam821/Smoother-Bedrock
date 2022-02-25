f3ab589018c92851d397d36f2186496b10279efc
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-02-25 17:15:09 -0500

    Update mod to Minecraft 1.18.2 release candidate 1

--- src/main/java/io/github/haykam821/smootherbedrock/mixin/LayerTransitionBlockSourceAccessor.java
+++ src/main/java/io/github/haykam821/smootherbedrock/mixin/LayerTransitionBlockSourceAccessor.java
@@ -1,10 +1,12 @@
 package io.github.haykam821.smootherbedrock.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import net.minecraft.block.BlockState;
 
+@Pseudo
 @Mixin(targets = "net.minecraft.class_5842", remap = false)
 public interface LayerTransitionBlockSourceAccessor {
 	@Accessor("field_35137")
